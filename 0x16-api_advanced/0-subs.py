790b76443990443d0a48293997bcc81ff806cfe3
AlinafeICK <alinafeick@gmail.com>
2024-08-16 03:15:56 +0200

    adhered to PEP 8 coding style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,6 +13,6 @@ def number_of_subscribers(subreddit):
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
-        return 0
+        return (0)
     results = response.json().get("data")
-    return results.get("subscribers")
+    return (results.get("subscribers"))
