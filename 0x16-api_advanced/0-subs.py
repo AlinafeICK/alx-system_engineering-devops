82e87d7513670165d20397666a5c305f550c9ac9
AlinafeICK <alinafeick@gmail.com>
2024-08-16 03:36:49 +0200

    removed whitespaces

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """
 Function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
-
 If an invalid subreddit is given, the function should return 0.
 """
 
@@ -11,11 +10,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    
+    Queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
     Args:
         subreddit (str): The name of the subreddit to query.
-    
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
