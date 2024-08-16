853a2a9c38ed33e1f31197e5a219a60f2fafaf84
AlinafeICK <alinafeick@gmail.com>
2024-08-16 03:27:34 +0200

    PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,8 @@
 """
 Function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
+
+If an invalid subreddit is given, the function should return 0.
 """
 
 import requests
@@ -10,7 +11,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
+    Function that queries the Reddit API.
+    
     - If not a valid subreddit, return 0.
     """
     req = requests.get(
