efb3647df0869bfe823fb83cd74fd77c2022453c
AlinafeICK <alinafeick@gmail.com>
2024-08-16 03:38:49 +0200

    PEP8 adherence

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" raddit api"""
+"""Function that queries the reddit api"""
 
 import json
 import requests
 
 
 def count_words(subreddit, word_list, after="", count=[]):
-    """count all words"""
+    """Count all words"""
 
     if after == "":
         count = [0] * len(word_list)
@@ -52,4 +52,3 @@ def count_words(subreddit, word_list, after="", count=[]):
                     print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
-    
